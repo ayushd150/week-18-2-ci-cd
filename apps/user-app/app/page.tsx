816b597a759bda6892378522f3452acf7dd00af6
ayushd150 <ayushdbpc2005@gmail.com>
2025-08-07 21:03:05 +0530

    new changes

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,10 +2,14 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
+
+// testing
+
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
+    return
   } else {
     redirect('/api/auth/signin')
   }
